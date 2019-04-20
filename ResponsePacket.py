ad7ee69a7d397c0a533ad9e9a6dc0d4699989dde
Chu Cheuk Kiu <calvinchu26@gmail.com>
2019-04-20 14:05:17 +0800

    debugged, removed old functions for ResponsePacket.py

--- ResponsePacket.py
+++ ResponsePacket.py
@@ -97,12 +97,12 @@ class ResponsePacket:
         self.__payload = payload
 
     def responseCode(self):
-        if self.__responseCode = '':
+        if self.__responseCode == '':
             responseLineSplitted = self.__responseLine.split(' ')
             self.__responseCode = responseLineSplitted[1]
         return self.__responseCode
 
-    def getKeepLive(self, option=''): # TODO needs testing
+    def getKeepLive(self, option=''):
         line = ''
         for ss in self.__headerSplitted:
             if ss[0:len('keep-alive')].lower() == 'keep-alive':
@@ -164,66 +164,3 @@ class ResponsePacket:
 
     def getPayload(self):
         return self.__payload
-
-
-
-# def getLastModified(self):
-#     if self.__lastModified == '':
-#         lastModifiedLine = ''
-#         for ss in self.__headerSplitted:
-#             if ss[0:len('last-modified')].lower() == 'last-modified':
-#                 lastModifiedLine = ss
-#                 break
-#         if lastModifiedLine == '':
-#             self.__lastModified == 'nil'
-#         else:
-#             lastModifiedLineSplitted = lastModifiedLine.split(' ')
-#             lastModifiedLineSplitted[1] = lastModifiedLineSplitted[1][:-1] # drop comma in 'Mon,'
-#             self.__lastModified = lastModifiedLineSplitted[1:-1] # drop 'last-modified' and 'GMT'
-#     return self.__lastModified
-
-# def getTransferEncoding(self):
-#     if self.__transferEncoding == '':
-#         transferEncodingLine = ''
-#         for ss in self.__headerSplitted:
-#             if ss[0:len('transfer-encoding')].lower() == 'transfer-encoding':
-#                 transferEncodingLine = ss
-#                 break
-#         if transferEncodingLine == '':
-#             self.__transferEncoding = 'nil'
-#         else:
-#             transferEncodingLineSplitted = transferEncodingLine.split(' ')
-#             self.__transferEncoding = transferEncodingLineSplitted[1]
-#     return self.__transferEncoding
-
-# def getCacheControl(self):
-#     if self.__cacheControl == '':
-#         cacheControlLine = ''
-#         for ss in self.__headerSplitted:
-#             if ss[0:len('cache-control')].lower() == 'cache-control':
-#                 cacheControlLine = ss
-#                 break
-#         if cacheControlLine == '':
-#             self.__cacheControl = 'nil'
-#         else:
-#             cacheControlLineSplitted = cacheControlLine.split(' ')
-#             self.__cacheControl = cacheControlLineSplitted[1]
-#     return self.__cacheControl
-
-# def getTimeout(self):
-#     if self.__timeout == '':
-#         keepAliveLine = ''
-#         for ss in self.__headerSplitted:
-#             if ss[0:len('keep-alive')].lower() == 'keep-alive':
-#                 keepAliveLine = ss
-#                 break
-#         if keepAliveLine == '':
-#             self.__timeout = -1
-#         else:
-#             keepAliveLineSplitted = keepAliveLine.split(' ')
-#             timeoutStr = keepAliveLineSplitted[1]
-#             timeoutStr = timeoutStr[len('timeout='):]
-#             if timeoutStr[-1] == ',':
-#                 timeoutStr = timeoutStr[:-1]
-#             self.__timeout = int(timeoutStr)
-#     return self.__timeout
