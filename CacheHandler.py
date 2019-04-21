9e7dadc6cda1fda84f1b14380193873ce704742f
Chu Cheuk Kiu <calvinchu26@gmail.com>
2019-04-21 23:04:33 +0800

    combined cache thread

--- CacheHandler.py
+++ CacheHandler.py
@@ -3,6 +3,17 @@ import os # remove file os.remove(filename)
 from RequestPacket import RequestPacket
 from ResponsePacket import ResponsePacket
 
+
+
+
+#  ██  ██       ██████  █████   ██████ ██   ██ ███████     ██   ██  █████  ███    ██ ██████  ██      ███████ ██████
+# ████████     ██      ██   ██ ██      ██   ██ ██          ██   ██ ██   ██ ████   ██ ██   ██ ██      ██      ██   ██
+#  ██  ██      ██      ███████ ██      ███████ █████       ███████ ███████ ██ ██  ██ ██   ██ ██      █████   ██████
+# ████████     ██      ██   ██ ██      ██   ██ ██          ██   ██ ██   ██ ██  ██ ██ ██   ██ ██      ██      ██   ██
+#  ██  ██       ██████ ██   ██  ██████ ██   ██ ███████     ██   ██ ██   ██ ██   ████ ██████  ███████ ███████ ██   ██
+
+
+
 class CacheHandler:
     '''
     cache handler offers only static functions
@@ -270,3 +281,47 @@ class CacheHandler:
         for subpart in cacheFileNameSplitted:
             cacheFileNameFH += subpart + '/'
         return cacheFileNameFH[:-1], cacheFileNameSplitted
+
+
+
+
+
+
+#  ██  ██       ██████  █████   ██████ ██   ██ ███████     ████████ ██   ██ ██████  ███████  █████  ██████
+# ████████     ██      ██   ██ ██      ██   ██ ██             ██    ██   ██ ██   ██ ██      ██   ██ ██   ██
+#  ██  ██      ██      ███████ ██      ███████ █████          ██    ███████ ██████  █████   ███████ ██   ██
+# ████████     ██      ██   ██ ██      ██   ██ ██             ██    ██   ██ ██   ██ ██      ██   ██ ██   ██
+#  ██  ██       ██████ ██   ██  ██████ ██   ██ ███████        ██    ██   ██ ██   ██ ███████ ██   ██ ██████
+
+
+
+
+import threading
+from CacheHandler import CacheHandler
+
+class CacheThread(threading.Thread):
+    '''
+    thread to cache the response
+
+    Members:
+
+        __rqp:                  request packet
+
+        __rsp:                  response packet to be cached, regardless of cacheability
+
+    Constructor:
+
+        default:                set rqp, rsp
+
+    Functions:
+
+        run:                    call CacheHandler.cacheResponse(rqp, rsp)
+    '''
+
+    def __init__(self, rqp, rsp):
+        threading.Thread.__init__(self)
+        self.__rqp = rqp
+        self.__rsp = rsp
+
+    def run(self):
+        CacheHandler.cacheResponse(self.__rqp, self.__rsp)
